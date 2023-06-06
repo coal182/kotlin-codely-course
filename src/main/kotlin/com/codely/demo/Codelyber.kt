7ef290caafc5fb776c7bfd4d81b478f354d9b10a
Cristian Martín <cristian.martin@es.stroeer-labs.com>
2023-06-06 15:31:42 +0200

    kotlin scopes

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,12 +1,35 @@
 package com.codely.demo
 
 import java.time.LocalDate
+import java.time.Period
+import kotlin.system.exitProcess
 
 fun main() {
     println("Please enter a date with format <yyyy-mm-dd>")
-    val readLine: String? = null
-    readLine?.let {
-        val input = LocalDate.parse(readLine)
-        println("You wrote $input ${input.dayOfWeek}")
+    supportNullableString(readLine()).takeUnless {
+        // returns null if the condition is true /-> takeIf would be in the opposite way
+        it.isNullOrEmpty() || it.isNullOrBlank()
+    }?.let {
+        // like map
+        LocalDate.parse(it)
+    }.apply {
+        // to make mutations
+        if(this == null) {
+            println("The date is not valid")
+            exitProcess(1)
+        }
+    }?.also {
+        // side effect
+        println("You wrote $it")
+    }.run {
+        // returns the inside block execution result
+        with( Period.between(this, LocalDate.now())){
+            // to avoid declaring variables
+            println("The difference between the date you wrote and today is ${this.years} years")
+        }
     }
+
+
 }
+
+fun supportNullableString(line: String?) = line
